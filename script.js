ffa5cd0d795be3088d9c0be738a4f22b84b43878
nxtcoreee3 <oliversvuxenkonto@gmail.com>
2026-03-15 19:46:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,8 +55,7 @@ const GAMES = [
    thumb: 'assets/polytrack.png',
    url: 'https://nxtcoreee3.github.io/Polytrack/',
    desc: 'Drive and race against your older records.'
-   isNew: true
-}
+   }
 ];
 
 /* --- Utilities --- */
